b3479cbdbcbe4fdd883502dc965aafd86fe018cf
superiorcamel <willrevell@hotmail.com>
2018-06-16 20:30:28 -0600

    oops

--- Project Atlas/Assets/Scripts/global_ship.cs
+++ Project Atlas/Assets/Scripts/global_ship.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class global_ship : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
