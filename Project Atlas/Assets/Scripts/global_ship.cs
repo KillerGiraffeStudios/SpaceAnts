77531c6282105fa02891df82d29758dbe1e98b07
mkp003 <mkp003@mail.usask.ca>
2018-06-16 21:00:26 -0600

    added base attributes for the ship and created the room script.

--- Project Atlas/Assets/Scripts/global_ship.cs
+++ Project Atlas/Assets/Scripts/global_ship.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class global_ship : MonoBehaviour {
 
+    // The ship's current health
+    private int health;
+
+    // The current happiness level of the population
+    private int happiness;
+
+    // Array of the rooms contained in the ship
+    //private room rooms[][];
+
 	// Use this for initialization
 	void Start () {
-		
+        this.health = 100;
+        this.happiness = 50;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    
 }
